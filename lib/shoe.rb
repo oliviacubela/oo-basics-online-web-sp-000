ded5e1d293bf1ee54acca5731e916502f767308f
Olivia Cubela <oliviacubela@gmail.com>
2020-06-19 18:47:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -46,14 +46,15 @@
 # end
 
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
   def initialize(brand)
     @brand = brand
   end
 
-  def cobble
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def cobble()
+    self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 end
